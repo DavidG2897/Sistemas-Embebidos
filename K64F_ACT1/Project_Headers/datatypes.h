ca92fcda93b2c64f95f834cffd25a8d73f921ea5
David <david.g.2897@gmail.com>
2018-08-24 19:09:18 -0500

    Update datatypes.h

--- K64F_ACT1/Project_Headers/datatypes.h
+++ K64F_ACT1/Project_Headers/datatypes.h
@@ -3,6 +3,7 @@
 
 #define TASKS_LIMIT 10
 #define NUMBER_OF_TASKS 3
+#define CONTEXT_SIZE 8
 #define __ASM __asm 	/*!< asm keyword for GNU Compiler */ 
 #define __INLINE inline /*!< inline keyword for GNU Compiler */ 
 #define __STATIC_INLINE static inline 
@@ -13,12 +14,17 @@ typedef unsigned char bool;
 typedef unsigned char u8b;
 typedef unsigned long u32b;
 enum STATES {IDLE,READY,RUN,WAIT};
-struct{
+typedef struct{
 	u8b PRIORITY;
 	bool AUTOSTART;
+	bool INTERRUPTED;
 	u32b RETURN_ADDR;
+	u32b CONTEXT[CONTEXT_SIZE];
+	u32b SP_FOR_TASK;
 	void (*TASK_INITIAL_ADDR)();
 	enum STATES STATE;
-} Tasks[TASKS_LIMIT];
+} Task;
+
+Task Tasks[NUMBER_OF_TASKS];
 
 #endif /* DATATYPES_H_ */
