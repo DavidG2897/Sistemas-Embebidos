bda24dc44cdfb0d9604bf026b352203181fc9fa5
David <david.g.2897@gmail.com>
2018-08-24 19:07:48 -0500

    Update main.c

--- K64F_ACT1/Sources/main.c
+++ K64F_ACT1/Sources/main.c
@@ -2,28 +2,42 @@
 #include "os.h"
 #include "datatypes.h"
 
-char c;
+char a,b,c;
 
 void taskA(){
-	char a = 5;
-	char b = 10;
-	activateTask(1);		//User-defined index for task B
-	c = a + b;
+	a = 5;
+	b = 10;
+	activateTask(1,FALSE);		//User-defined index for task B
+	c = c + a + b;
 	terminateTask();
 }
 
 void taskB(){
 	c++;
+	//chainTask(2);
 	terminateTask();
 }
+
 void taskC(){
 	c*=2;
+	terminateTask();
+}
+
+void PORTA_IRQHandler(void){	//ISR pushes XPSR, PC, LR, R12, R3, R2, R1, R0 in said order
+	saveContext();
+	PORTA_PCR1|=1<<24;		//w1c ISF (Interrupt Status Flag) por PTA1
+	activateTask(1,TRUE);
 }
 
 int main(void){
 	
-	//volatile Task tasks[number_of_tasks];		//User defined tasks size
+	SIM_SCGC5=1<<9;			//Enable clock for PORT A
 	
+	PORTA_PCR1=(1<<8)+(9<<16);	//PTA1 as GPIO and interrupt enable on rising-edge
+	
+	NVICICER1|=1<<(59%32);	//Clear PORT A interrupt flag
+	NVICISER1|=1<<(59%32); 	//Set IRQ Vector for PORT A (Vector 59)
+		
 	//Task A
 	Tasks[0].PRIORITY=0;
 	Tasks[0].AUTOSTART=TRUE;
