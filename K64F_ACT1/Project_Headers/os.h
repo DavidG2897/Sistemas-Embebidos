688571970bde29ba70b8414bbc9fbf67bf70c67a
David <david.g.2897@gmail.com>
2018-08-24 19:09:38 -0500

    Update os.h

--- K64F_ACT1/Project_Headers/os.h
+++ K64F_ACT1/Project_Headers/os.h
@@ -3,9 +3,11 @@
 #include "datatypes.h"
 
 void OS_init(void);
-void activateTask(u8b);
+void activateTask(u8b,bool);
+void saveContext(void);
 void chainTask(u8b);
 void terminateTask(void);
 __attribute__( ( always_inline ) ) __STATIC_INLINE uint32_t __get_LR(void);
+__attribute__( ( always_inline ) ) __STATIC_INLINE uint32_t __get_SP(void);
 
 #endif /* OS_H_ */
