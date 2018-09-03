936117a17ef88d3474654035452f6b971dc625e0
David <david.g.2897@gmail.com>
2018-09-02 19:35:34 -0500

    Update os.h

--- K64F_ACT1/Project_Headers/os.h
+++ K64F_ACT1/Project_Headers/os.h
@@ -3,11 +3,15 @@
 #include "datatypes.h"
 
 void OS_init(void);
-void activateTask(u8b,bool);
+void activateTask(u8b, bool);
 void saveContext(void);
 void chainTask(u8b);
 void terminateTask(void);
-__attribute__( ( always_inline ) ) __STATIC_INLINE uint32_t __get_LR(void);
-__attribute__( ( always_inline ) ) __STATIC_INLINE uint32_t __get_SP(void);
+void createTask(u8b,u8b,bool,void*);
+void createAlarm(u8b,u8b,u32b,bool,bool);
+void createMailbox(u8b,void*,void*);
+bool writeMailbox(u8b,u32b);		//Return 1 if write unsuccesfull, return 0 otherwise
+u32b readMailbox(u8b,u32b*);		//Return 1 if read unsuccesfull, return 0 otherwise
+void wait(void);					//Sends currently running task to WAIT state
 
 #endif /* OS_H_ */
